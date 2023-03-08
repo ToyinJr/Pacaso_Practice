b087b7961556e08e30991d8db851f05dd5ddac02
ToyinJr <juniortoyin@yahoo.com>
2023-03-08 13:33:33 +0100

    Sample commit

--- js/script1.js
+++ js/script1.js
@@ -20,18 +20,6 @@ hamburger.addEventListener("click", function() {
 });
 
 
-// var splide = new Splide( '.splide', {
-//   type   : 'loop',
-//   perPage: 2,
-//   perMove: 1,
-// } );
-
-
-// var splide = new Splide( '.splide', {
-//   type: 'loop',
-//   direction: 'ttb',
-//   height   : '30rem',
-// } ); 
 
 var splide;
 
@@ -49,29 +37,9 @@ splide = new Splide( '.splide', {
   } );
   
 
-// splide = new Splide( '.splide', {
-//     type: 'loop',
-//     direction: 'ttb',
-//     height   : '30rem',
-//   } ); 
-
 splide.mount();
 
-var rrr = document.getElementById('rrr');
 
-// var x = window.matchMedia("(max-width: 768px)")
-
-// function myFunction(x) {
-//     if (x.matches) {
-//       rrr.setAttribute("class", "red");
-//     } 
-//      else {
-//       rrr.setAttribute("class", "blue");
-//   }
-// }
-
-//   myFunction(x) // Call listener function at run time
-//   x.addListener(myFunction) // Attach listener function on state changes
 
 
 
